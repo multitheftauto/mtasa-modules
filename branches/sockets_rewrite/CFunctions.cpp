a3bcfac39a5750d6c17d5a18faa5e82761c5ad5e
Gamesnert <Gamesnert@e906ef02-791a-11de-99de-5969a5ca275b>
2010-06-22 19:27:56 +0000

    Added rewrite branch for the current Sockets module. About time for some major cleanup. Hopefully makes Linux compatible again as well.

---
+++ branches/sockets_rewrite/CFunctions.cpp
@@ -0,0 +1,119 @@
+/*********************************************************
+*
+*  Multi Theft Auto: San Andreas - Deathmatch
+*
+*  ml_base, External lua add-on module
+*
+*  Copyright © 2003-2008 MTA.  All Rights Reserved.
+*
+*  Grand Theft Auto is © 2002-2003 Rockstar North
+*
+*  THE FOLLOWING SOURCES ARE PART OF THE MULTI THEFT
+*  AUTO SOFTWARE DEVELOPMENT KIT AND ARE RELEASED AS
+*  OPEN SOURCE FILES. THESE FILES MAY BE USED AS LONG
+*  AS THE DEVELOPER AGREES TO THE LICENSE THAT IS
+*  PROVIDED WITH THIS PACKAGE.
+*
+*********************************************************/
+
+#include "CFunctions.h"
+
+int CFunctions::sockOpen(lua_State* luaVM)
+{
+    if ( luaVM )
+    {
+        // Make sure host is a string, and port is a number
+        if (lua_type(luaVM, 1) == LUA_TSTRING && lua_type(luaVM, 2) == LUA_TNUMBER)
+        {
+            // Put the host in a string, and the port in an unsigned short
+            string strHost        = lua_tostring(luaVM, 1);
+            unsigned short usPort = static_cast<unsigned short>(atoi(lua_tostring(luaVM, 2)));
+
+            // Create the socket
+            CSocket* pSocket = new CSocket(luaVM, strHost, usPort);
+            void* pUserdata  = pSocket->GetUserdata();
+
+            // The socket has got a userdata value if successfully created. It doesn't otherwise
+            if (pUserdata == NULL)
+            {
+                SAFE_DELETE(pSocket);
+                lua_pushboolean(luaVM, false);
+                return 1;
+            }
+
+            // Add the socket to the Pulse list
+            CSocketManager::SocketAdd(pSocket);
+
+            // Return the userdata
+            lua_pushlightuserdata(luaVM, pUserdata);
+            return 1;
+        }
+    }
+
+    lua_pushboolean(luaVM, false);
+    return 1;
+}
+
+int CFunctions::sockWrite(lua_State *luaVM)
+{
+    if ( luaVM )
+    {
+        // Make sure the socket is an userdata value, and data is a string
+        if (lua_type(luaVM, 1) == LUA_TLIGHTUSERDATA && lua_type(luaVM, 2) == LUA_TSTRING)
+        {
+            // Prepare variables
+            void* pUserdata  = lua_touserdata(luaVM, 1);
+            CSocket* pSocket = NULL;
+
+            // Get the socket from the userdata. Gets stored in pSocket. If failed, returns false
+            if (CSocketManager::GetSocket(pSocket, pUserdata))
+            {
+                // Try to send the data
+                string strData = lua_tostring(luaVM, 2);
+                bool bSuccess  = pSocket->Send(strData);
+
+                // Return whether or not it was successful
+                lua_pushboolean(luaVM, bSuccess);
+                return 1;
+            }
+        }
+    }
+
+    lua_pushboolean(luaVM, false);
+    return 1;
+}
+
+int CFunctions::sockClose(lua_State *luaVM)
+{
+    if ( luaVM )
+    {
+        // Make sure the socket is an userdata value
+        if (lua_type(luaVM, 1) == LUA_TLIGHTUSERDATA)
+        {
+            // Prepare vars
+            void* pUserdata  = lua_touserdata(luaVM, 1);
+            CSocket* pSocket = NULL;
+
+            // Get the socket
+            if (CSocketManager::GetSocket(pSocket, pUserdata))
+            {
+                // Remove it
+                CSocketManager::SocketRemove(pSocket);
+
+                lua_pushboolean(luaVM, true);
+                return 1;
+            }
+        }
+    }
+
+    lua_pushboolean(luaVM, false);
+    return 1;
+}
+
+void CFunctions::AddEvent(lua_State *luaVM, const string& strEventName)
+{
+    CLuaArguments args;
+    args.PushString(strEventName.c_str());
+    args.PushBoolean(true);
+    args.Call(luaVM, "addEvent");
+}
---
+++ branches/sockets_rewrite/CSocket.cpp
@@ -0,0 +1,227 @@
+#include "CSocket.h"
+
+CSocket::CSocket(lua_State *luaVM, const string& strHost, const unsigned short& usPort)
+{
+    // Prepare variables
+    m_bConnected = false;
+    m_strHost    = strHost;
+    m_usPort     = usPort;
+    m_pLuaVM     = luaVM;
+    m_pUserdata  = NULL;
+    
+    // Prepare data for connection (cancel on failure)
+    if (!ProcessTargetLocation(strHost, usPort))
+        return;
+    
+    // Create the socket, and put it in non-blocking mode
+    m_pSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    // Exit if the socket failed to create
+    if (m_pSocket == ERR_INVALID_SOCKET)
+        return;
+
+    // Set the socket to non-blocking mode
+    SetNonBlocking();
+
+    // Make the socket connect
+    if (connect(m_pSocket, (sockaddr*)&m_sSockAddr, sizeof(m_sSockAddr)) != ERR_CONNECT_SUCCESS)
+    {
+        // If the connection failed, check why
+        int iError = GetLastSocketError();
+
+        // If the error is ERR_WOULD_BLOCK (meaning it's in progress) then ignore
+        if (iError != ERR_WOULD_BLOCK)
+        {
+            // Error, so unload the socket and exit the function
+            CloseSocket();
+            return;
+        }
+    }
+
+    // Create userdata, for identification
+    m_pUserdata  = lua_newuserdata(luaVM, 128);
+}
+
+CSocket::~CSocket()
+{
+    // Close the socket, if it exists, and trigger the closed event
+    if (m_pSocket)
+    {
+        CloseSocket();
+        TriggerEvent("onSockClosed");
+    }
+}
+
+bool CSocket::Send(const string& data)
+{
+    // Make sure the socket exists
+    if (!m_pSocket)
+        return false;
+
+    // Send the data and return whether it was successful
+    return (send(m_pSocket, data.c_str(), data.length(), 0) != ERR_SEND_FAILURE);
+}
+
+bool CSocket::DoPulse()
+{
+    // Make sure the socket exists before taking action
+    if (m_pSocket)
+    {
+        // Create a buffer for catching received data
+        // (1 byte larger than receive limit, because another character has to be added)
+        char chBuffer[SOCK_RECV_LIMIT + 1];
+
+        // Receive the data
+        int iLength = recv(m_pSocket, chBuffer, SOCK_RECV_LIMIT, 0);
+
+        // Check if there were any errors
+        int iError = GetLastSocketError();
+
+        // Check if the socket just connected. If connection failed, return false
+        if (!m_bConnected && (HandleConnection(iError) == ERR_CONNECT_FAILURE))
+            return false;
+
+        // If connected, handle data processing
+        if (m_bConnected)
+        {
+            // Process data if there is any
+            if (iLength > 0)
+            {
+                // Add a NULL at the end of the data, or the data will appear corrupted
+                chBuffer[iLength] = '\0';
+
+                TriggerEvent("onSockData",chBuffer);
+            }
+            else if (iError != ERR_NO_ERROR && iError != ERR_WOULD_BLOCK)
+            {
+                // An error has occured, so time to kill the socket
+                m_bConnected = false;
+                return false;
+            }
+        }
+    }
+    else
+        // If the socket doesn't exist, well, error?
+        return false;
+
+    // If the call makes it up till here, it has been a huge success! Cake and true as a reward!
+    return true;
+}
+
+bool CSocket::IsConnected()
+{
+    // If there's no socket, then we don't have a connection
+    if (!m_pSocket)
+        return false;
+
+    // If there is, show whether there is a connection
+    return m_bConnected;
+}
+
+void* CSocket::GetUserdata()
+{
+    return m_pUserdata;
+}
+
+bool CSocket::ProcessTargetLocation(const string& strHost, const unsigned short& usPort)
+{
+    memset(&m_sSockAddr, 0, sizeof(m_sSockAddr));
+    m_sSockAddr.sin_family = AF_INET;
+    m_sSockAddr.sin_port   = htons(usPort);
+
+    hostent* Hostent;
+    unsigned long IP = inet_addr(strHost.c_str());
+
+    if (IP != INADDR_NONE)
+    {
+        m_sSockAddr.sin_addr.s_addr = IP;
+        return true;
+    }
+    else
+    {
+        Hostent = gethostbyname(strHost.c_str());
+        if (Hostent == NULL)
+        {
+            return false;
+        }
+        else
+        {
+            memcpy(&(m_sSockAddr.sin_addr), Hostent->h_addr_list[0], 4);
+            return true;
+        }
+    }
+}
+
+void CSocket::SetNonBlocking()
+{
+    // Multi-platform function for setting the socket to non-blocking mode
+
+#ifdef WIN32
+    u_long ulMode = 1;
+    ioctlsocket(m_pSocket, FIONBIO, &ulMode);
+#else
+    fcntl(m_pSocket, F_SETFL, fcntl(m_pSocket, F_GETFL) | O_NONBLOCK);
+#endif
+}
+
+int CSocket::GetLastSocketError()
+{
+    // Multi-platform function for getting the last socket error
+
+#ifdef WIN32
+    return WSAGetLastError();
+#else
+    return errno;
+#endif
+}
+
+void CSocket::CloseSocket()
+{
+    // Close the connection before closing the socket
+    shutdown(m_pSocket, 1);
+
+#ifdef WIN32
+    closesocket(m_pSocket);
+#else
+    close(m_pSocket);
+#endif
+    
+    // Unset the socket variable, so there's no mistaking there
+    m_pSocket = NULL;
+}
+
+int CSocket::HandleConnection(const int& iError)
+{
+    if (iError == ERR_NO_ERROR || iError == ERR_WOULD_BLOCK)
+    {
+        m_bConnected = true;
+        TriggerEvent("onSockOpened");
+        return 1;
+    }
+    else if (iError == ERR_NOT_CONNECTED)
+    {
+        return 0;
+    }
+    else
+    {
+        printf("Could not connect due to error: %i",iError); // TEMP DEBUG
+        return -1;
+    }
+}
+
+void CSocket::TriggerEvent(const string eventName, const string arg1)
+{
+    CLuaArguments args;
+    args.PushString(eventName.c_str());
+
+    lua_getglobal(m_pLuaVM, "root");
+    CLuaArgument RootElement(m_pLuaVM, -1);
+
+    args.PushUserData(RootElement.GetLightUserData());
+    args.PushUserData(m_pUserdata);
+
+    if (arg1.length() > 0)
+        args.PushString(arg1.c_str());
+
+    args.Call(m_pLuaVM, "triggerEvent");
+}
